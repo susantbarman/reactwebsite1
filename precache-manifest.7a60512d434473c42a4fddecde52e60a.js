6447602e6da1f1a2d59bbf340982a63b38f030e3
Susant Barman <41489071+susantbarman@users.noreply.github.com>
2020-10-17 14:22:38 +0530

    This is my first react website
    This site converts HTML to React website.

---
+++ precache-manifest.7a60512d434473c42a4fddecde52e60a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bee7fecfe46b48375c10211bb9eec0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec768706bd97aa1b678",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "0b5798547c2d2afe495c",
+    "url": "/static/js/2.c16f82fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c16f82fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec768706bd97aa1b678",
+    "url": "/static/js/main.aeeaaa3a.chunk.js"
+  },
+  {
+    "revision": "859271f5d7652c1df88a",
+    "url": "/static/js/runtime-main.9dbff38a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
